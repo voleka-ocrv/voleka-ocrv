72627b8b0a3a171c7d20dcd0d5c92741b9820ee5
voleka-ocrv <novka_07@mail.ru>
2020-02-16 19:04:42 +0300

    log

--- script.js
+++ script.js
@@ -7,9 +7,11 @@ function switchColor(obj) {
     if (id === 'redButton'){
         document.getElementById('redButton').value = 'orangeButton';
         id = 'orangeButton';
+        console.log(`after switch red-orange id= ${id}`);
     }
     else if (id === 'orangeButton'){
         document.getElementById('orangeButton').value = 'redButton';        
         document.getElementById('orangeButton').id = 'redButton';
+        console.log(`after switch orange-red id= ${id}`);
     }
 }
