be95ad8ac9b089dd835488d5f512e1e14cb10f83
voleka-ocrv <novka_07@mail.ru>
2020-02-16 18:38:40 +0300

    switch color

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert( 'Всем привет!' );
+function switchColor(id) {
+
+    // alert( 'Всем привет!' );
+    alert(id.value);
+    if (id === 'redButton'){
+        document.getElementById('redButton').value = 'orangeButton';
+        document.getElementById('redButton').id = 'orangeButton';
+    }
+    else if (id === 'orangeButton'){
+        document.getElementById('orangeButton').value = 'redButton';        
+        document.getElementById('orangeButton').id = 'redButton';
+    }
+}
