e7f82371207470ea266a38e3c8f34409481d833f
voleka-ocrv <novka_07@mail.ru>
2020-02-16 19:18:53 +0300

    +

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 function switchColor(obj) {
-
-    // alert( 'Всем привет!' );
     let id = obj.id;
-    // alert(obj);
-    console.log(id);
-    if (id === 'redButton'){
-        document.getElementById('redButton').value = 'orangeButton';
-        document.getElementById('redButton').id = 'orangeButton';
-        console.log(`after switch red-orange id= ${document.getElementById('redButton').id}`);
-    }
-    else if (id === 'orangeButton'){
-        document.getElementById('orangeButton').value = 'redButton';        
-        document.getElementById('orangeButton').id = 'redButton';
-        console.log(`after switch orange-red id= ${id}`);
-    }
+    console.log(`На входе: obj= ${obj}, id= ${id}`);
+    // if (id === 'redButton'){
+    //     document.getElementById('redButton').value = 'orangeButton';
+    //     document.getElementById('redButton').id = 'orangeButton';
+    //     console.log(`after switch red-orange id= ${document.getElementById('redButton').id}`);
+    // }
+    // else if (id === 'orangeButton'){
+    //     document.getElementById('orangeButton').value = 'redButton';        
+    //     document.getElementById('orangeButton').id = 'redButton';
+    //     console.log(`after switch orange-red id= ${id}`);
+    // }
 }
