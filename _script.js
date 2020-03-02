19f36d8f9798aa4e0a202e512f5cf9e08938c91d
voleka-ocrv <novka_07@mail.ru>
2020-03-02 09:39:24 +0300

    byID

--- _script.js
+++ _script.js
@@ -1,13 +1,24 @@
-let box = document.getElementById('userParams'),
-div = document.createElement('div'),
-text = document.createTextNode('Тут был я!');
+// let box = document.getElementById('userParams'),
+// div = document.createElement('div'),
+// text = document.createTextNode('Тут был я!');
 
-div.classList.add('skills');
+// div.classList.add('skills');
 
-document.body.appendChild(div);
+// document.body.appendChild(div);
 
-document.body.removeChild();
+// document.body.removeChild();
 
-document.body.innerHTML = 'Текст появился по волшебству';
+// document.body.innerHTML = 'Текст появился по волшебству';
 
-console.log(div);
+// console.log(div);
+let btn = document.getElementsByTagName('button');
+
+// btn[1].onclick = function() {
+//     alert('Вы нажали вторую кнопку');
+// };
+
+console.log("btn");
+console.log(btn);
+console.log(btn[1]);
+console.log(btn.item(1));
+console.log(document.getElementById('btn1'))
